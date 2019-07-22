53eef50be50cea909dbab63412330f93a534a08f
Rorywork <rorywork95@outlook.com>
2019-07-22 15:26:58 +0100

    Added templates folder with home.html file inside

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-@app.route('/<name>')
-def index(name): 
-    return '<h1>Hello {}!</h1>'.format(name)
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
