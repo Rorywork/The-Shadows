3892c23916519b4be5ff7874a2b80daa8ac3b2f7
Rorywork <rorywork95@outlook.com>
2019-07-23 16:53:37 +0100

    Trying to deploy

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,7 +12,12 @@ def about():
     return render_template('about.html')
 
 
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    app.run(debug=False, port=environ.get("PORT", 5000))    
+  
 
 # test code c
