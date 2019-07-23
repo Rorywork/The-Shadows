bc78c0109854ed654e5f0f9f2270739b213db3b4
Rorywork <rorywork95@outlook.com>
2019-07-23 17:51:57 +0100

    Trying to deploy

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def about():
 
 if __name__ == '__main__':
     from os import environ
-    app.run(debug=False, port=environ.get("PORT", 5000))    
+    app.run(debug=False, host='0.0.0.0',port=environ.get("PORT", 5000))    
   
 
 # test code c
