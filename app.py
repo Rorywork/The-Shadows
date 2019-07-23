eca3783fed336c55d8bdc9469903c26e7ce7c3e0
Rorywork <rorywork95@outlook.com>
2019-07-23 21:27:39 +0100

    Mongo now connected and working

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.onject.id import ObjectId
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'myTestDB'
-app.config["MONGO_URI"] = 'mongodb+srv://root:B4dmintonC0d3@myfirstcluster-tdray.mongodb.net/myFirstMDB?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://root:B4dmintonC0d3@myfirstcluster-tdray.mongodb.net/myTestDB?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 
-@app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template('tasks.html', tasks=mongo.db.tasks.find())
+#@app.route('/get_tasks')
+#def get_tasks():
+#    return render_template('tasks.html', tasks=mongo.db.tasks.find())
 
 @app.route('/')
 def index():
@@ -22,15 +21,15 @@ def index():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('tasks.html', tasks=mongo.db.myFirstMDB.find())
 
-
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', debug=True)
 
 if __name__ == '__main__':
-    from os import environ
-    app.run(debug=False, host='0.0.0.0',port=environ.get("PORT", 5000))    
+    app.run(debug=True)
+
+#if __name__ == '__main__':
+#    from os import environ
+#    app.run(debug=False, host='0.0.0.0',port=environ.get("PORT", 5000))    
   
 
 # test code c
