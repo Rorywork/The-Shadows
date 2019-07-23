170a0bb3b84c8407a00f0bf579048b0b03cfe7bb
Rorywork <rorywork95@outlook.com>
2019-07-23 20:41:29 +0100

    Testing connection to MongoDB created file called tasks in html folder and edited app.py file

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import flask_pymongo
+from bson.onject.id import ObjectId
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'myTestDB'
+app.config{"MONGO_URI"} = 'mongodb+srv://root:B4dmintonC0d3@myfirstcluster-tdray.mongodb.net/myTestDB?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template('tasks.html', tasks=mongo.db.tasks.find())
 
 @app.route('/')
 def index():
