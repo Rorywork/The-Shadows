fd1294e846d9675321fc7ddd2dbc552541544480
Rorywork <rorywork95@outlook.com>
2019-07-24 15:33:39 +0100

    Added route for profile page

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:B4dmintonC0d3@myfirstcluster-tdray
 mongo = PyMongo(app)
 
 
+
+
 #@app.route('/get_tasks')
 #def get_tasks():
 #    return render_template('tasks.html', tasks=mongo.db.tasks.find())
@@ -24,12 +26,47 @@ def about():
     return render_template('tasks.html', tasks=mongo.db.myFirstMDB.find())
 
 
+
+@app.route('/upload')
+def upload():
+    return '''
+        <form method="POST" action="/create" enctype="multipart/form-data">
+            <input type="text" name="username">
+            <input type="file" name="profile_image">
+            <input type="submit">
+        </form>
+    '''
+
+@app.route('/create', methods=['POST'])
+def create():
+    if 'profile_image' in request.files:
+        profile_image = request.files['profile_image']
+        mongo.save_file(profile_image.filename, profile_image)
+        mongo.db.users.insert({'username' : request.form.get('username'), 'profile_image_name' : profile_image.filename})
+    
+    return 'Done!'
+
+@app.route('/file/<filename>')
+def file(filename):
+    return mongo.send_file(filename)
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = mongo.db.users.find_one_or_404({'username' : username})
+    return f'''
+        <h1>{username}</h1>
+        <img src="{url_for('file', filename=user['profile_image_name'])}">
+    '''
+
+
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 if __name__ == '__main__':
     app.run(debug=True)
+
+# The above is required for local development but
+# needs to be commented out to run in Heroku and replaced with the below code
 
 #if __name__ == '__main__':
 #    from os import environ
 #    app.run(debug=False, host='0.0.0.0',port=environ.get("PORT", 5000))    
-  
-
-# test code c
