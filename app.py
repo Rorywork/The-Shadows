0e2189f1ef282a9ce0457cd812bc712b14a0821a
Rorywork <rorywork95@outlook.com>
2019-08-08 22:56:40 +0100

    Create class RegisterForm in app.py and an app route for register

--- app.py
+++ app.py
@@ -107,7 +107,22 @@ def editphotodetails(photoid):
 
     return render_template('edit-photo.html', form=form)    
 
+    class RegisterForm(Form):
+        name = StringField('Name', [validators.Length(min=1, max=50)])
+        username = StringField('Username', [validators.Length(min=4, max=25)])
+        email = StringField('Email', [validators.Length(min=6, max=50)])
+        password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm' message='Passwords do not match')
+            ])
+        confirm = PasswordField('Confirm Password')
 
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+
+        return render_template('register.html', form=form)
 
 
 
