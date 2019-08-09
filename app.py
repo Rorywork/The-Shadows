b37a770a5509b66fb9f6fff2a20f4bc16bbb9bb8
Rorywork <rorywork95@outlook.com>
2019-08-09 23:02:39 +0100

    Made app.py work with environmental variables

--- app.py
+++ app.py
@@ -5,24 +5,27 @@ from bson.objectid import ObjectId
 from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
+from os import environ
 
 app = Flask(__name__)
-app.secret_key =  os.environ.get('SECRET_KEY')
+app.secret_key = os.environ.get('SECRET_KEY')
 
 app.config["MONGO_DBNAME"] = 'myTestDB'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
+
 def is_logged_in(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        if 'logged_in' in session: 
+        if 'logged_in' in session:
             return f(*args, **kwargs)
         else:
             flash('Unauthorized, Please login', 'danger')
             return redirect(url_for('login'))
     return wrap
+
 
 @app.route('/')
 def index():
@@ -40,10 +43,12 @@ def create():
     if 'image_file' in request.files:
         image_file = request.files['image_file']
         mongo.save_file(image_file.filename, image_file)
-        mongo.db.photos.insert({'username' : request.form.get('username'), 'image_file' : image_file.filename, 'image_name' : request.form.get('image_name'), 'image_description' : request.form.get('image_description'), 'image_category' : request.form.get('image_category'), 'image_rotation' : request.form.get('image_rotation')})
+        mongo.db.photos.insert({'username': request.form.get('username'), 'image_file': image_file.filename, 'image_name': request.form.get(
+            'image_name'), 'image_description': request.form.get('image_description'), 'image_category': request.form.get('image_category'), 'image_rotation': request.form.get('image_rotation')})
         #photo2show = mongo.db.photos.find_one_or_404({"_id" : ObjectId(photoid)})
         flash("The photo was uploaded to the site.")
-    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1),]))
+    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1), ]))
+
 
 @app.route('/file/<filename>')
 def file(filename):
@@ -53,7 +58,7 @@ def file(filename):
 @app.route('/photo/<image_name>')
 def getImage(image_name):
     print(image_name)
-    photo = mongo.db.photos.find_one_or_404({'image_name' : image_name})
+    photo = mongo.db.photos.find_one_or_404({'image_name': image_name})
     print(photo)
     return f'''
         
@@ -61,63 +66,68 @@ def getImage(image_name):
         
     '''
 
+
 @app.route('/showphotos')
 def showphotos():
-    # Show all photo sorted by creation date - from the _id    
-    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1),]))    
+    # Show all photo sorted by creation date - from the _id
+    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1), ]))
 
 
 @app.route('/showphotosbycategory/<category>')
 def showphotosbycategory(category):
     flash("Showing photographs for the category: {}".format(category))
-    return render_template('showphotos.html', photos=mongo.db.photos.find({"image_category" : category }).sort([('_id', -1),]))
+    return render_template('showphotos.html', photos=mongo.db.photos.find({"image_category": category}).sort([('_id', -1), ]))
 
 
 @app.route('/deletephoto/<photoid>')
 def deletephoto(photoid):
-    photo2delete = mongo.db.photos.find_one_or_404({"_id" : ObjectId(photoid)})
-    mongo.db.photos.delete_one({"_id" : ObjectId(photoid)})
-    flash("The photograph {} has been deleted.".format(photo2delete['image_name']))
+    photo2delete = mongo.db.photos.find_one_or_404({"_id": ObjectId(photoid)})
+    mongo.db.photos.delete_one({"_id": ObjectId(photoid)})
+    flash("The photograph {} has been deleted.".format(
+        photo2delete['image_name']))
     # return f'''
     #     <h1>{photo2delete['image_description']} DELETED.</h1>
     # '''
-    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1),]))    
+    return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1), ]))
+
 
 class PhotoForm(Form):
     username = StringField('User Name', [validators.Length(min=1, max=50)])
     image_name = StringField('Image Name', [validators.Length(min=1, max=200)])
-    image_description = StringField('Image Description', [validators.Length(min=1, max=200)])
+    image_description = StringField(
+        'Image Description', [validators.Length(min=1, max=200)])
     image_file = StringField('Image File', [validators.Length(min=1, max=500)])
     #image_category = SelectField('Image Category',choices=['People','Animals','Natural', 'Urban'])
 
 
-
 @app.route('/editphotodetails/<photoid>', methods=['GET', 'POST'])
 def editphotodetails(photoid):
-    #Get photo to edit
-    photo2edit = mongo.db.photos.find_one_or_404({"_id" : ObjectId(photoid)})
-    #Get form to edit
+    # Get photo to edit
+    photo2edit = mongo.db.photos.find_one_or_404({"_id": ObjectId(photoid)})
+    # Get form to edit
     form = PhotoForm(request.form)
-    #Pre-populate form with values
+    # Pre-populate form with values
     print(photo2edit['username'])
     form.username.data = photo2edit['username']
     form.image_name.data = photo2edit['image_name']
     form.image_description.data = photo2edit['image_description']
     form.image_file.data = photo2edit['image_file']
     #form.image_category.data = photo2edit['image_category']
-    if request.method =='POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         username = request.form['username']
         image_name = request.form['image_name']
         image_description = request.form['image_description']
-        #image_file = request.form['image_file'] ..... cannot capture this due to HTML security
+        # image_file = request.form['image_file'] ..... cannot capture this due to HTML security
         #image_category = request.form['image_category']
         # Update the record
-        mongo.db.photos.update_one({"_id" : ObjectId(photoid)} , { '$set' : {'username': username}})
+        mongo.db.photos.update_one({"_id": ObjectId(photoid)}, {
+                                   '$set': {'username': username}})
         # ..... more to come.....
         flash('Photo {} has been updated.'.format(image_name), 'success')
-        return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1),]))    
+        return render_template('showphotos.html', photos=mongo.db.photos.find().sort([('_id', -1), ]))
 
-    return render_template('edit-photo.html', form=form)    
+    return render_template('edit-photo.html', form=form)
+
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
@@ -129,6 +139,7 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
@@ -137,13 +148,15 @@ def register():
         email = form.email.data
         username = form.username.data
         password = sha256_crypt.encrypt(str(form.password.data))
-        #insert into the MongoDB
-        mongo.db.users.insert({'name' : name, 'email' : email, 'username' : username, 'password' : password})
-        
+        # insert into the MongoDB
+        mongo.db.users.insert(
+            {'name': name, 'email': email, 'username': username, 'password': password})
+
         flash('You are now registered and can log in', 'success')
 
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -151,11 +164,11 @@ def login():
         username = request.form['username']
         password_candidate = request.form['password']
 
-        doc_count = mongo.db.users.count_documents({'username' : username})
-        
+        doc_count = mongo.db.users.count_documents({'username': username})
+
         if doc_count > 0:
 
-            result = mongo.db.users.find_one_or_404({'username' : username})
+            result = mongo.db.users.find_one_or_404({'username': username})
             password = result['password']
 
             if sha256_crypt.verify(password_candidate, password):
@@ -175,12 +188,12 @@ def login():
 
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
     session.clear()
     flash('You are now logged out', 'success')
-    return redirect (url_for('login'))
-
+    return redirect(url_for('login'))
 
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -191,5 +204,5 @@ def logout():
 # needs to be commented out to run in Heroku and replaced with the below code
 
 if __name__ == '__main__':
-   from os import environ
-   app.run(debug=False, host='0.0.0.0',port=environ.get("PORT", 5000))    
+    app.run(debug=os.environ.get('DEBUG'), host=os.environ.get(
+        'IP'), port=os.environ.get('PORT'))
