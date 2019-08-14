a3695001e4ac8f313eb08e8e019111679d33684c
Rorywork <rorywork95@outlook.com>
2019-08-14 14:56:52 +0100

    Built the like button

--- app.py
+++ app.py
@@ -16,6 +16,11 @@ APP.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 MONGO = PyMongo(APP)
 
+# Pagination defaults
+search = False
+per_page = 5
+bs_version = 4
+
 
 def is_logged_in(f):
     @wraps(f)
@@ -47,9 +52,12 @@ def create():
         MONGO.save_file(image_file.filename, image_file)
         MONGO.db.photos.insert({'username': session['username'], 'image_file': image_file.filename, 'image_name': request.form.get(
             'image_name'), 'image_description': request.form.get('image_description'), 'image_category': request.form.get('image_category'), 'image_rotation': request.form.get('image_rotation')})
-        #photo2show = mongo.db.photos.find_one_or_404({"_id" : ObjectId(photoid)})
-        flash("The photo was uploaded to the site.")
-    return render_template('showphotos.html', photos=MONGO.db.photos.find().sort([('_id', -1), ]))
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        photos = MONGO.db.photos
+        allphotos = photos.find().skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
+        pagination = Pagination(page=page, per_page=5, total=allphotos.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
+        flash("The photo was uploaded to the site.", 'success')
+    return render_template('showphotos.html', photos= allphotos, pagination= pagination)
 
 
 @APP.route('/file/<filename>')
@@ -71,39 +79,45 @@ def getImage(image_name):
 
 @APP.route('/showphoto/<photoid>')
 def showphoto(photoid):
-    return render_template('showphotos.html', photos=MONGO.db.photos.find({"_id": ObjectId(photoid)}))    
+     # Show individual photo
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    photos = MONGO.db.photos
+    onephoto = photos.find({"_id": ObjectId(photoid)}).skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
+    pagination = Pagination(page=page, per_page=5, total=onephoto.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
+    return render_template('showphotos.html', photos= onephoto, pagination= pagination)
 
 @APP.route('/showphotos')
 def showphotos():
     # Show all photo sorted by creation date - from the _id
-    search = False
-    q = request.args.get('q')
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    per_page = 5
-    bs_version = 4
     photos = MONGO.db.photos
-    allphotos = photos.find().skip((page - 1) * per_page).limit(per_page)
+    allphotos = photos.find().skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
     pagination = Pagination(page=page, per_page=5, total=allphotos.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
-
     return render_template('showphotos.html', photos= allphotos, pagination= pagination)
 
 
 @APP.route('/showphotosbycategory/<category>')
 def showphotosbycategory(category):
+    # Show photos filtered by category and sorted by creation date 
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    photos = MONGO.db.photos
+    catphotos = photos.find({"image_category": category}).skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
+    pagination = Pagination(page=page, per_page=5, total=catphotos.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
     flash("Showing photographs for the category: {}".format(category))
-    return render_template('showphotos.html', photos=MONGO.db.photos.find({"image_category": category}).sort([('_id', -1), ]))
+    return render_template('showphotos.html', photos= catphotos, pagination= pagination)
 
 
 @APP.route('/deletephoto/<photoid>')
 def deletephoto(photoid):
     photo2delete = MONGO.db.photos.find_one_or_404({"_id": ObjectId(photoid)})
     MONGO.db.photos.delete_one({"_id": ObjectId(photoid)})
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+    photos = MONGO.db.photos
+    allphotos = photos.find().skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
+    pagination = Pagination(page=page, per_page=5, total=allphotos.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
     flash("The photograph {} has been deleted.".format(
-        photo2delete['image_name']))
-    # return f'''
-    #     <h1>{photo2delete['image_description']} DELETED.</h1>
-    # '''
-    return render_template('showphotos.html', photos=MONGO.db.photos.find().sort([('_id', -1), ]))
+        photo2delete['image_name']), 'success')
+    return render_template('showphotos.html', photos= allphotos, pagination= pagination)
 
 
 class PhotoForm(Form):
@@ -139,18 +153,27 @@ def editphotodetails(photoid):
             '$set': {'username': username, 'image_name': image_name, 'image_description': image_description, "image_category": image_category }})
 
         flash('Photo {} has been updated.'.format(image_name), 'success')
-        return render_template('showphotos.html', photos=MONGO.db.photos.find().sort([('_id', -1), ]))
+        page = request.args.get(get_page_parameter(), type=int, default=1)
+        photos = MONGO.db.photos
+        allphotos = photos.find().skip((page - 1) * per_page).sort([('_id', -1), ]).limit(per_page)
+        pagination = Pagination(page=page, per_page=5, total=allphotos.count(), search=search, record_name='photos', bs_version=bs_version, css_framework='bootstrap', show_single_page=False)
+        return render_template('showphotos.html', photos= allphotos, pagination= pagination)
 
     return render_template('edit-photo.html', form=form)
 
 @APP.route('/postcomment/<photoid>', methods=['POST'])
 def postcomment(photoid):
-    print(photoid)
-    print(request.form.get('comment'))
+    #print(photoid)
+    #print(request.form.get('comment'))
     MONGO.db.photos.update_one({'_id': ObjectId(photoid)}, {'$push': {'comments': request.form.get('comment')}}, upsert=True)
-    flash("Your comment has been added to the photo.")
+    flash("Your comment has been added to the photo.", 'success')
     return redirect(url_for('showphoto', photoid=photoid))
 
+@APP.route('/addlike/<photoid>', methods=['POST'])
+def addlike(photoid):
+    MONGO.db.photos.update_one({'_id': ObjectId(photoid)}, {'$inc': {'likes': 1}}, upsert=True)
+    flash("Your 'like' has been added to the photo.", 'success')
+    return redirect(url_for('showphoto', photoid=photoid))
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
